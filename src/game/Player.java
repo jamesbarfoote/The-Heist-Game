09ee4897ae886787511f37d163594ea1cf136dcd
James Barfoote <james.barfoote@gmail.com>
2015-09-26 14:34:53 +1200

    added player class

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,12 +1,77 @@
 package game;
 
+import java.awt.Point;
+
 public class Player {
-	//Room
-	//Robber or guard(can't pick up money, can open all doors) enum
-	//Score
-	//Weapon
-	//Location(coords of the square with current room)
+	private Room room;
+	private Weapon weapon;
+	private int score;
+	private Point location;//Location(coords of the square with current room)
+	private Type t;
 	
-	//Talks to client
+	//Robber or guard(can't pick up money, can open all doors) enum
+	public enum Type{
+		robber, guard
+	}
+	
+	public Player(Room r, Weapon w, Point p, Type t)
+	{
+		this.room = r;
+		this.weapon = w;
+		this.location = p;
+		this.t = t;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public Room getRoom()
+	{
+		return room;
+	}
+	
+	public Weapon getWeapon()
+	{
+		return weapon;
+	}
+	
+	public Point getLocation()
+	{
+		return location;
+	}
+	
+	public Type getPlayerType()
+	{
+		return t;
+	}
+	
+	public void updateScore(int s)
+	{
+		score = s;
+	}
+	
+	public void updateRoom(Room r)
+	{
+		room = r;
+	}
+	
+	public void changeWeapon(Weapon w)
+	{
+		weapon = w;
+	}
+	
+	public void updateLocation(Point p)
+	{
+		location = p;
+	}
+	
+	public void setgetPlayerType(Type t)
+	{
+		this.t = t;
+	}
+	
+	
 
 }
