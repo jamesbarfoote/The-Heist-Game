69d4d3a33100df9307c6543b9ecd27bbdb4d68c7
Lachlan Lee <altairlachlan@gmail.com>
2015-10-06 02:59:59 +1300

    Refactored ImmovableItem and MovableItem to better reflect what they represent
    Now named InteractableItem and NonInteractableItem

---
+++ src/game/items/NonInteractableItem.java
@@ -0,0 +1,12 @@
+package game.items;
+
+
+/**
+ * Abstraction of the Immovable objects in the bank. This encompasses all objects that cannot be interacted with
+ * A.K.A. set dressing for the rooms.
+ * @author Lachlan
+ *
+ */
+public abstract class NonInteractableItem implements Item{
+
+}
