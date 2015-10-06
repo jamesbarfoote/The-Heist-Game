2056f81d4f0dba53e9a2ce9a0cde3760db886d84
Lachlan Lee <altairlachlan@gmail.com>
2015-10-06 13:22:35 +1300

    Made NonInteractableItem into a non-abstract class in preparation for using an enum to determine type
    Added Money and Item fields and get methods to Container

--- src/game/items/Container.java
+++ src/game/items/Container.java
@@ -2,11 +2,14 @@ package game.items;
 
 import java.awt.Point;
 
+import game.Money;
 import game.Room;
 
 public class Container extends InteractableItem{
 
 	private Point position;
+	private Money money; //Null if no money in container
+	private InteractableItem item; //Null if no item in container
 	
 	public Container(Room room, Point position) {
 		super(room);
@@ -15,6 +18,19 @@ public class Container extends InteractableItem{
 	
 	public Point getPosition(){
 		return position;
+	}
+	
+	public int getMoney(){
+		return money.getAmount();
+	}
+	
+	public InteractableItem getItem(){
+		return item;
+	}
+	
+	public void itemRemovedFromContainer(){
+		money = null;
+		item = null;
 	}
 
 }
