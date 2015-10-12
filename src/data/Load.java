d2682a3232209093415c5920875e6ecffdf2e7a4
nickbotica <nickbotica@outlook.com>
2015-10-12 18:48:23 +1300

    Updated

--- src/data/Load.java
+++ src/data/Load.java
@@ -1,6 +1,6 @@
 package data;
 
-import game.items.Item;
+import game.*;
 
 import java.awt.Point;
 import java.io.File;
@@ -9,16 +9,22 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Load {
 
-	public static void loadFromXML(String fileName) {
+	private static final String ITEM = "item";
+	private static final String TYPE = "type";
+	private static final String ROOM = "room";
+	private static final String DESK = "desk";
+	private static final String MONEY = "money";
+
+	public static Room loadFromXML(String fileName, Room room) {
 
 		try {
-
 			File file = new File(fileName);
 
 			DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance()
@@ -26,85 +32,71 @@ public class Load {
 
 			Document doc = docBuilder.parse(file);
 
-			// TODO: remove debugging printlns
-			System.out.println("Root element :"
-					+ doc.getDocumentElement().getNodeName());
+			Element gameNode = doc.getDocumentElement();
 
-			// sanity check, then parse
-			if (doc.hasChildNodes()) {
-				parseFile(doc.getChildNodes());
-			}
+			getRooms(gameNode, room);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+
+		return room;
 	}
 
-	private static void parseFile(NodeList nodeList) {
+	// <room name="hall">
+	private static void getRooms(Element gameNode, Room room) {
+		// get all the rooms
+		NodeList roomsNodeList = gameNode.getElementsByTagName(ROOM);
 
-		for (int count = 0; count < nodeList.getLength(); count++) {
+		// design change, only one 'room' per map
+		Element roomNode = (Element) roomsNodeList.item(0);
 
-			Node tempNode = nodeList.item(count);
+		// pass room to get the items in the room
+		getRoomItems(roomNode, room);
+	}
 
-			// make sure it's element node.
-			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+	// <item = type"money">
+	private static void getRoomItems(Element roomNode, Room room) {
+		// get all the items in the room
+		NodeList roomItemsNodeList = roomNode.getChildNodes();
 
-				// get node name
-				String typeNode = tempNode.getNodeName();
+		for (int count = 0; count < roomItemsNodeList.getLength(); count++) {
+			// if the node is an element
+			if (roomItemsNodeList.item(count) instanceof Element) {
+				Element itemNode = (Element) roomItemsNodeList.item(count);
 
-				if (tempNode.hasAttributes()) {
-
-					// get attributes names and values
-					NamedNodeMap nodeMap = tempNode.getAttributes();
-
-					for (int i = 0; i < nodeMap.getLength(); i++) {
-
-						Node node = nodeMap.item(i);
-
-						switch (typeNode) {
-						case "item":
-							itemNode(node);
-							break;
-						case "money":
-							moneyNode(node);
-							break;
-						case "door":
-							doorNode(node);
-							break;
-						}
-					}
+				String type = itemNode.getAttribute(TYPE);
+				switch (type) {
+				case (MONEY):
+					room.addItem(addMoney(itemNode));
+					break;
+				case (DESK):
+					room.addItem(addDesk(itemNode));
+					break;
 				}
 			}
+
 		}
 	}
 
-	/**
-	 * Parse item.
-	 *
-	 * @param itemNode
-	 * @return - Item from xml
-	 */
-	private static Item itemNode(Node itemNode) {
+	private static Money addMoney(Element moneyNode) {
 
-		// get attributes names and values
-		NamedNodeMap nodeMap = itemNode.getAttributes();
-		itemNode
+		NodeList moneyNodeList = moneyNode.getChildNodes();
 
-		// 1st child node of item
-		// <name>
-		Node node = nodeMap.item(0);
-		String name = node.getNodeValue();
-		// <pos>
-		node = nodeMap.item(1);
-		String pos = node.getNodeValue();
-		Point point = stringToPoint(pos);
+		// pos
+		Node node = moneyNodeList.item(1);
+		Element element = (Element) node;
+		String pos = element.getTextContent();
 
-		
-		
-		
-		
+		// amount
+		node = moneyNodeList.item(3);
+		element = (Element) node;
+		String amount = element.getTextContent();
 
-		return item;
+		Money money = new Money(Integer.parseInt(amount), null,
+				stringToPoint(pos));
+
+		return money;
 	}
 
 	/**
@@ -119,8 +111,11 @@ public class Load {
 		int x = Integer.parseInt(pos.split(",")[0]);
 		int y = Integer.parseInt(pos.split(",")[1]);
 
-		Point temp = new Point(x, y);
-		return temp;
+		Point point = new Point(x, y);
+		return point;
 	}
 
+	public static void main(String[] args) {
+		loadFromXML("game_save_001.xml");
+	}
 }
