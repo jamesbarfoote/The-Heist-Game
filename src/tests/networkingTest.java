cab2fd8f1c428da504432ef810b52013821dddb0
James Barfoote <james.barfoote@gmail.com>
2015-10-14 12:57:48 +1300

    created test package
    Added in networking test class

---
+++ src/tests/networkingTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class networkingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void canConnect()
+	{
+		
+	}
+	
+	@Test
+	public void setsID()
+	{
+		
+	}
+	
+	
+
+}
