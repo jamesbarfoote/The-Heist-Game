a35fbb6d317bd47c4c3e16cfdd747c08a0528779
James Barfoote <james.barfoote@gmail.com>
2015-10-14 13:46:35 +1300

    added 3 tests to the networking

--- src/tests/networkingTest.java
+++ src/tests/networkingTest.java
@@ -2,27 +2,76 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.io.IOException;
+
 import org.junit.Test;
 
+import game.Player;
+import game.items.Weapon;
+import networking.Client;
+import networking.Server;
+
 public class networkingTest {
+	Thread myThread;
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void canConnect()
+	public void canConnect() throws IOException, InterruptedException
 	{
-		
+		Player player = new Player("Bob", new Weapon("Badass", true), new Point(1,1), game.Player.Type.robber);
+		startServer();
+		Client c = new Client(43200, "localhost", player);
+		myThread.interrupt();
 	}
-	
+
 	@Test
 	public void setsID()
 	{
-		
+		Player player = new Player("Bob", new Weapon("Badass", true), new Point(1,1), game.Player.Type.robber);
+		startServer();
+		Client c = null;
+		try {
+			c = new Client(43200, "localhost", player);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		player = c.getPlayer();
+		assertTrue(c.getID() == 0);
+		myThread.interrupt();
 	}
 	
-	
+	@Test
+	public void returnedListCorrect()
+	{
+		Player player = new Player("Bob", new Weapon("Badass", true), new Point(1,1), game.Player.Type.robber);
+		startServer();
+		Client c = null;
+		try {
+			c = new Client(43200, "localhost", player);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(c.getPlayers().size() == 1);
+		myThread.interrupt();
+	}
+
+	public void startServer()
+	{
+		Server myRunnable = new Server(); //Start the server in a new thread
+		myThread = new Thread(myRunnable);
+		myThread.setDaemon(true); 		
+		myThread.start(); 
+	}
+
+
 
 }
